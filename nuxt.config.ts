60d58a335c793ff1965c2e20b3380eaf1e322d47
tanerijun <tanerivince@gmail.com>
2023-06-04 13:37:35 +0800

    Add lint and format scripts and disable lintOnStart

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true
-  },
-
-  modules: ["@nuxtjs/eslint-module"]
+	devtools: {
+		enabled: true,
+	},
+	eslint: {
+		lintOnStart: false,
+	},
+	modules: ["@nuxtjs/eslint-module"],
 })
