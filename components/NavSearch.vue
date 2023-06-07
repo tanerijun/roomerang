37ddf3a5f850510adbd4ef7bd3298b98f66e191c
tanerijun <tanerivince@gmail.com>
2023-06-07 22:17:18 +0800

    Basic markup for navbar

--- components/NavSearch.vue
+++ components/NavSearch.vue
@@ -1,10 +1,12 @@
 <template>
+	<!-- TODO: consider styling this as daisy button -->
 	<div
-		class="flex w-full cursor-pointer flex-row items-center justify-between rounded-full border py-2 shadow-sm transition hover:shadow-md md:w-auto"
+		class="flex h-10 cursor-pointer flex-row items-center justify-between rounded-full border shadow-sm transition hover:shadow-md md:w-auto"
 	>
 		<span class="px-6">Anywhere</span>
-		<span class="border-l border-r px-6">Anyweek</span>
+		<span class="border-l border-r px-6">Any week</span>
 		<div class="flex items-center gap-3 px-6">
+			<!-- TODO: make text color gray -->
 			<span>Add guests</span>
 			<div class="flex h-8 w-8 items-center justify-center rounded-full border border-green-200">
 				<Icon name="mingcute:search-2-line" />
---
+++ components/NavSearchMobile.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="btn-outline btn">Test</div>
+</template>
