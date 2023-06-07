1375da436931a829b28a1c22feefe53ee7016be9
tanerijun <tanerivince@gmail.com>
2023-06-07 12:27:36 +0800

    Add icons to Navbar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -9,12 +9,15 @@
 			<div>
 				<span>Test</span>
 			</div>
-			<div class="flex gap-2">
-				<button class="btn btn-sm">Rent your house</button>
-				<button class="btn btn-sm">
+			<div class="flex">
+				<button class="btn btn-sm btn-ghost rounded-full">Rent your house</button>
+				<button class="btn btn-sm btn-ghost btn-circle mr-3">
 					<Icon name="mingcute:earth-line" />
 				</button>
-				<button class="btn btn-sm">User account</button>
+				<button class="btn btn-sm btn-outline rounded-full">
+					<Icon name="mingcute:menu-line" />
+					<Icon name="mingcute:user-4-fill" size="2em" />
+				</button>
 			</div>
 		</div>
 	</header>
