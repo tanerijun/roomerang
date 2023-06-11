703df43abd7bd00644d084a13ca13a9e6fb24170
tanerijun <tanerivince@gmail.com>
2023-06-11 23:23:29 +0800

    Add Mobile version of NavActionsToggle

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,7 +1,15 @@
 <template>
-	<header class="h-20 flex items-center justify-between">
-		<NavLogo />
-		<NavActions />
-		<NavUserActions />
+	<header class="h-20 flex items-center">
+		<!-- Large screen -->
+		<div class="hidden flex-1 items-center justify-between sm:flex">
+			<NavLogo />
+			<NavActionsToggle />
+			<NavUserActions />
+		</div>
+
+		<!-- Mobile -->
+		<div class="block flex-1 sm:hidden">
+			<NavActionsToggleMobile />
+		</div>
 	</header>
 </template>
