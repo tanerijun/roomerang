2babda14ae0af6a09ae07d8a29abc9a6543a3987
tanerijun <tanerivince@gmail.com>
2023-06-14 22:39:08 +0800

    Change menu contents

--- components/nav/NavUserActions.vue
+++ components/nav/NavUserActions.vue
@@ -7,7 +7,7 @@
 		>
 			<i class="i-mingcute-world-line text-xl" />
 		</button>
-		<HeadlessMenu as="div" class="relative">
+		<HeadlessMenu as="div" class="relative border border-red-300">
 			<HeadlessMenuButton
 				class="ml-3 flex items-center gap-1.5 border border-gray-200 rounded-full px-2 py-0.5 transition-shadow hover:shadow-md"
 				aria-label="user actions"
@@ -15,15 +15,18 @@
 				<i class="i-mingcute-menu-line text-xl" />
 				<i class="i-mingcute-user-4-fill text-4xl" />
 			</HeadlessMenuButton>
-			<HeadlessMenuItems class="absolute right-0 mt-2 w-56 origin-top-right">
+			<HeadlessMenuItems class="absolute right-0 mt-2 w-max flex flex-col origin-top-right">
 				<HeadlessMenuItem v-slot="{ active }">
-					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Account settings </a>
+					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Sign up </a>
 				</HeadlessMenuItem>
 				<HeadlessMenuItem v-slot="{ active }">
-					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Documentation </a>
+					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Log in </a>
 				</HeadlessMenuItem>
-				<HeadlessMenuItem disabled>
-					<span class="opacity-75">Invite a friend (coming soon!)</span>
+				<HeadlessMenuItem v-slot="{ active }">
+					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Rent your house </a>
+				</HeadlessMenuItem>
+				<HeadlessMenuItem v-slot="{ active }">
+					<a :class="{ 'bg-blue-500': active }" href="/account-settings"> Help </a>
 				</HeadlessMenuItem>
 			</HeadlessMenuItems>
 		</HeadlessMenu>
